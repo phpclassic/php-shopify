9c6d4bf5afdaab4f881a946346c7c5e771a060a9
Tareq Mahmood <tareqtms@yahoo.com>
2018-08-28 12:32:50 +0600

    Report test parameters.

--- tests/ReportTest.php
+++ tests/ReportTest.php
@@ -4,5 +4,18 @@ namespace PHPShopify;
 
 class ReportTest extends TestSimpleResource
 {
+    /**
+     * @inheritDoc
+     */
+    public $postArray = array(
+        "name" => "A new app report",
+        "shopify_ql" => "SHOW total_sales BY country FROM order SINCE -1m UNTIL today ORDER BY total_sales",
+    );
 
+    /**
+     * @inheritDoc
+     */
+    public $putArray = array(
+        "name" => "A new app report - updated",
+    );
 }

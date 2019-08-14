9358bd2b87764c0e26e7e2b7aa9f31c5ba176239
Thomas Hondema <thomas.hondema@multisafepay.com>
2019-08-14 18:43:28 +0200

    added draftorder basics

---
+++ lib/DraftOrder.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * @author Thomas Hondema <thomashondema@live.com>
+ * Created at 8/14/19 18:28 PM UTC+02:00
+ *
+ * @see https://help.shopify.com/api/reference/draftorder Shopify API Reference for DraftOrder
+ */
+
+namespace PHPShopify;
+
+
+
+/**
+ * --------------------------------------------------------------------------
+ * DraftOrder -> Custom actions
+ * --------------------------------------------------------------------------
+ * @method array send_invoice()     Send the invoice for a DraftOrder
+ * @method array complete()         Complete a DraftOrder
+ *
+ */
+class DraftOrder extends ShopifyResource
+{
+    /**
+     * @inheritDoc
+     */
+    protected $resourceKey = 'draft_order';
+
+    /**
+     * @inheritDoc
+     */
+    protected $customPostActions = array(
+        'send_invoice',
+        'complete',
+    );
+}

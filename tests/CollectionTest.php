c232b69e2aa42928f84267dca437dc9aa89eab34
Tareq Mahmood <tareqtms@yahoo.com>
2020-03-07 11:06:33 +0600

    Disable test for Collection resource

--- tests/CollectionTest.php
+++ tests/CollectionTest.php
@@ -4,5 +4,13 @@ namespace PHPShopify;
 
 class CollectionTest extends TestSimpleResource
 {
+    /**
+     * Test get resource
+     *
+     * Post and list not available for Collection resource so testGet not possible
+     */
+    public function testGet()
+    {
 
+    }
 }

6a114af6763217d0b32920c09b4a4a55663dca08
Matthew Crigger <mcrigger@thexroadz.com>
2020-07-17 12:43:33 -0400

    Author and Links
    Updated authoring and help links.

--- lib/Payouts.php
+++ lib/Payouts.php
@@ -2,7 +2,7 @@
 /**
  * Created by PhpStorm.
  * @author Robert Jacobson <rjacobson@thexroadz.com>
- * Created at 11/11/19 12:26 PM UTC-05:00
+ * @author Matthew Crigger <mcrigger@thexroadz.com>
  *
  * @see https://help.shopify.com/en/api/reference/shopify_payments/payout Shopify API Reference for Shopify Payment Payouts
  */
--- lib/Transactions.php
+++ lib/Transactions.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * @author Robert Jacobson <rjacobson@thexroadz.com>
+ * @author Matthew Crigger <mcrigger@thexroadz.com>
+ *
+ * @see https://help.shopify.com/en/api/reference/shopify_payments/transaction Shopify API Reference for Shopify Payment Transactions
+ */
 
 namespace PHPShopify;
 

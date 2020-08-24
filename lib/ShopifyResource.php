d0e7fe7e9e046af71966ca55b9597d3790ed580c
Saumil Nagariya <saumil.nagariya@gmail.com>
2020-08-24 20:55:30 +0530

    Created function to generate account_activation_link for a specific customer

--- lib/ShopifyResource.php
+++ lib/ShopifyResource.php
@@ -540,6 +540,11 @@ abstract class ShopifyResource
         if (isset($responseArray['errors'])) {
             $message = $this->castString($responseArray['errors']);
 
+            //check account already enabled or not
+            if($message=='account already enabled'){
+                return array('account_activation_url'=>false);
+            }
+            
             throw new ApiException($message, CurlRequest::$lastHttpCode);
         }
 

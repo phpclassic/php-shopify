6df94536df2d08e89132af4cd0153716c3942607
Tareq Mahmood <tareqtms@yahoo.com>
2023-02-13 09:35:25 +0600

    Update Default API Version Number

--- lib/ShopifySDK.php
+++ lib/ShopifySDK.php
@@ -235,7 +235,7 @@ class ShopifySDK
     /**
      * @var string Default Shopify API version
      */
-    public static $defaultApiVersion = '2022-07';
+    public static $defaultApiVersion = '2023-01';
 
     /**
      * Shop / API configurations
